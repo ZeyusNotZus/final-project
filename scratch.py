6b0f656045a06dc7e2fc00014e1115962277adb8
mgfilipino <mgfilipino@up.edu.ph>
2025-11-05 16:04:41 +0800

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,169 @@
+import os 
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+world1 = [
+    ["+", "T", "T", "T", "T", "T", "T", "T", "T"],
+    ["T", ".", ".", ".", ".", ".", ".", ".", "T"],
+    ["T", "_", "_", ".", ".", ".", ".", ".", "T"],
+    ["T", ".", ".", ".", ".", ".", "+", ".", "T"],
+    ["T", "x", "*", ".", "L", "T", ".", ".", "T"],
+    ["T", ".", ".", ".", ".", ".", ".", ".", "T"],
+    ["T", ".", "~", "~", "R", ".", "R", ".", "."],
+    ["T", ".", "~", "~", ".", ".", ".", ".", "T"],
+    ["T", "T", "T", "T", "T", "T", "T", "T", "T"]
+]
+
+#Game Functions
+def reset():
+    global game_data, current_world, rocks
+    current_world = [row.copy() for row in world1]
+    game_data = {
+        "state": "playing",
+        "on": ".",
+        "inventory": ".",
+        "total_shrooms":  "".join(x for y in world1 for x in y).count("+"),
+        "shrooms": 0
+    }
+    rocks = {(r, c): "." for r, c in find_object(current_world, "R")}
+
+
+def find_object(world, rep):
+    positions = []
+    for r, row in enumerate(world):
+        for c, tile in enumerate(row):
+            if tile == rep:
+                if rep == "L":
+                    return r, c
+                elif rep == "R":
+                    positions.append((r, c))
+    if rep == "R":
+        return positions
+
+def player_move(pos, direction, world):
+    r, c = pos
+    dr, dc = direction
+    target_r, target_c = r + dr, c + dc
+
+    #Out of bounds
+    if not (0 <= target_r < n_rows and 0 <= target_c < n_cols):
+        return r, c, current_world[r][c]
+    
+    #Checking Target Tile
+    target_tile = current_world[target_r][target_c]
+
+    #Regular Movement
+    if target_tile in valid_tiles:
+        return target_r, target_c, target_tile
+    
+    #Chopping Trees
+    if target_tile == "T" and game_data["inventory"] == "x":
+        current_world[target_r][target_c] = "."
+        game_data["inventory"] = "."
+        return target_r, target_c, "."
+    
+    #Burning Trees
+    elif target_tile == "T" and game_data["inventory"] == "*":
+        def burn(r, c):
+            if 0 <= r < n_rows and 0 <= c < n_cols and world[r][c] == "T":
+                world[r][c] = "."
+                for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    burn(r + dr, c + dc)    
+
+        burn(target_r, target_c)
+        game_data["inventory"] = "."
+        return target_r, target_c, "."
+        
+    #When pushing rocks
+    if (0 <= target_r < n_rows) and (0 <= target_c < n_cols) and world[target_r][target_c] == "R":
+        if (0 <= target_r + dr < n_rows) and (0 <= target_c + dc < n_cols) and world[target_r + dr][target_c + dc] in (".", "_", "~"):
+            
+            #Pushing rock to water
+            if world[target_r + dr][target_c + dc] == "~":
+                current_world[target_r + dr][target_c + dc] = "_"
+                current_world[target_r][target_c] = rocks.pop((target_r, target_c))
+                return target_r, target_c, current_world[target_r][target_c]
+
+            #Pushing rock normally
+            elif world[target_r + dr][target_c + dc] in (".", "_"):
+                rocks[(target_r + dr, target_c + dc)] = current_world[target_r + dr][target_c + dc]
+                current_world[target_r + dr][target_c + dc] = "R"
+                current_world[target_r][target_c] = rocks.pop((target_r, target_c))
+                return target_r, target_c, current_world[target_r][target_c]
+        else:
+            return r, c, current_world[r][c]  
+
+    return r, c, current_world[r][c]  
+
+#Independent Variables
+current_world = [row.copy() for row in world1]
+player_input = {
+    "w": (-1,0),
+    "s": (1,0),
+    "a": (0,-1),
+    "d": (0,1),
+    "p": "pickup",
+    "!": "reset",
+    "q": "quit"
+}
+valid_tiles = (".", "+", "x", "~", "*", "_")
+pickup = ("x", "*")
+pushable = ("R")
+n_rows = len(current_world)
+n_cols = len(current_world[0])
+
+#Create World
+reset()
+#Quit condition. Should be placed somewhere else
+quit = False
+
+
+#Game Loop
+while True:
+    clear_screen()
+
+    print("\n".join("".join(row) for row in current_world))
+    print()
+    print(f"Shrooms: {game_data["shrooms"]}/{game_data["total_shrooms"]}")
+    print(f"Inventory: {game_data["inventory"]}")
+    print(f"Player tile: {game_data["on"]}\n")
+    
+    if game_data["state"] == "gameover":
+        if game_data["shrooms"] >= game_data["total_shrooms"]:
+            print("You Win!")
+        else:
+            print("Game Over\n")
+
+    if game_data["state"] == "playing":
+        action = input("Move (W, A, S, D) \nPickup (P)\nReset (!) \nQuit (Q): \n").lower()
+    elif game_data["state"] == "gameover":
+        action = input("Reset (!) \nQuit (Q): \n").lower()
+
+    for act in action:
+        if act not in player_input:
+            break
+        elif act == "q":
+            quit = True
+        elif act == "!":
+            reset()
+        elif game_data["state"] == "playing":
+            if act == "p" and game_data["on"] in pickup and game_data["inventory"] == ".":
+                game_data["inventory"] = game_data["on"]
+                game_data["on"] = "."
+            elif act != "p":
+                #This part can probably be but under the player move function
+                player_row, player_col = find_object(current_world, "L")
+                current_world[player_row][player_col] = game_data["on"]
+                new_r, new_c, game_data["on"] = player_move((player_row, player_col), player_input[act], current_world)
+                if game_data["on"] == "~":
+                    game_data["state"] = "gameover"
+                else:
+                    if game_data["on"] == "+":
+                        game_data["on"] = "."
+                        game_data["shrooms"] += 1
+                        if game_data["shrooms"] >= game_data["total_shrooms"]:
+                            game_data["state"] = "gameover"
+                    current_world[new_r][new_c] = "L"
+
+    if quit:
+        break
