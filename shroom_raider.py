fe17c554671fb86d91bc5a1a2e732408399268ca
ZeyusNotZus <zbflorida@up.edu.ph>
2025-11-14 02:22:41 +0800

    Add files via upload

--- shroom_raider.py
+++ shroom_raider.py
@@ -1,21 +1,8 @@
 import os 
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument("-f", "--file", help="Stage file")
-parser.add_argument("-m", "--moves", help="Moves")
-parser.add_argument("-o", "--output", help = "Output file")
-args = parser.parse_args()
-
-#Checking if a map file was given
-if args.file:
-    with open(args.file, encoding='utf-8') as f:
-        rows, cols = [int(x) for x in f.readline().strip().split()]
-        rest = [line.strip() for line in f if line.strip()]
-        world = "".join(rest)
-        world1 = [list(world[i * cols:(i + 1) * cols]) for i in range(rows)]
-else:
-    world1 = [
+"""Default world when no world is imported to game"""
+world1 = [
     ["R", "R", "R", "T", "T", "T", "T", "T", "T"],
     ["R", "+", "R", ".", ".", ".", ".", "L", "T"],
     ["R", "T", "R", ".", ".", ".", "+", ".", "T"],
@@ -25,19 +12,15 @@ else:
     ["T", "~", "*", ".", "T", "T", "T", ".", "T"],
     ["T", "+", "~", ".", ".", ".", ".", ".", "T"],
     ["T", "T", "T", "T", "T", "T", "T", "T", "T"]
-]
+    ]
 
-#Checking if moves are used in advance
-if args.moves:
-    move_queue = args.moves
-else:
-    move_queue = None
+# Game Functions
 
-
-#Game Functions
+"""Clears the terminal screen"""
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
+"""Resets game to initial state of world and initial game data values"""
 def reset():
     global game_data, current_world, rocks
     current_world = [row.copy() for row in world1]
@@ -50,7 +33,8 @@ def reset():
     }
     rocks = {(r, c): "." for r, c in find_object(current_world, "R")}
 
-
+"""Finds positions of the player and rocks in the world
+   player position is returned as r, c while rocks are returned as a list of tuples (r,c)"""
 def find_object(world, rep):
     positions = []
     for r, row in enumerate(world):
@@ -63,29 +47,33 @@ def find_object(world, rep):
     if rep == "R":
         return positions
 
+"""Movement of player in given direction and player interactions with game world"""
 def player_move(pos, direction, world):
+    global game_data, rocks, valid_tiles
     r, c = pos
     dr, dc = direction
     target_r, target_c = r + dr, c + dc
+    n_rows = len(world)
+    n_cols = len(world[0])
 
-    #Out of bounds
+    # Out of bounds
     if not (0 <= target_r < n_rows and 0 <= target_c < n_cols):
-        return r, c, current_world[r][c]
+        return r, c, world[r][c]
     
-    #Checking Target Tile
-    target_tile = current_world[target_r][target_c]
+    # Checking target tile
+    target_tile = world[target_r][target_c]
 
-    #Regular Movement
+    # Regular movement
     if target_tile in valid_tiles:
         return target_r, target_c, target_tile
     
-    #Chopping Trees
+    # Chopping trees
     if target_tile == "T" and game_data["inventory"] == "x":
-        current_world[target_r][target_c] = "."
+        world[target_r][target_c] = "."
         game_data["inventory"] = "."
         return target_r, target_c, "."
     
-    #Burning Trees
+    # Burning trees
     elif target_tile == "T" and game_data["inventory"] == "*":
         def burn(r, c):
             if 0 <= r < n_rows and 0 <= c < n_cols and world[r][c] == "T":
@@ -97,28 +85,28 @@ def player_move(pos, direction, world):
         game_data["inventory"] = "."
         return target_r, target_c, "."
         
-    #When pushing rocks
+    # When pushing rocks
     if (0 <= target_r < n_rows) and (0 <= target_c < n_cols) and world[target_r][target_c] == "R":
         if (0 <= target_r + dr < n_rows) and (0 <= target_c + dc < n_cols) and world[target_r + dr][target_c + dc] in (".", "_", "~"):
             
-            #Pushing rock to water
+            # Pushing rock to water
             if world[target_r + dr][target_c + dc] == "~":
-                current_world[target_r + dr][target_c + dc] = "_"
-                current_world[target_r][target_c] = rocks.pop((target_r, target_c))
-                return target_r, target_c, current_world[target_r][target_c]
+                world[target_r + dr][target_c + dc] = "_"
+                world[target_r][target_c] = rocks.pop((target_r, target_c))
+                return target_r, target_c, world[target_r][target_c]
 
-            #Pushing rock normally
+            # Pushing rock normally
             elif world[target_r + dr][target_c + dc] in (".", "_"):
-                rocks[(target_r + dr, target_c + dc)] = current_world[target_r + dr][target_c + dc]
-                current_world[target_r + dr][target_c + dc] = "R"
-                current_world[target_r][target_c] = rocks.pop((target_r, target_c))
-                return target_r, target_c, current_world[target_r][target_c]
+                rocks[(target_r + dr, target_c + dc)] = world[target_r + dr][target_c + dc]
+                world[target_r + dr][target_c + dc] = "R"
+                world[target_r][target_c] = rocks.pop((target_r, target_c))
+                return target_r, target_c, world[target_r][target_c]
         else:
-            return r, c, current_world[r][c]  
+            return r, c, world[r][c]  
 
-    return r, c, current_world[r][c]  
+    return r, c, world[r][c]  
 
-#Variables
+#V ariables
 current_world = [row.copy() for row in world1]
 player_input = {
     "w": (-1,0),
@@ -131,70 +119,107 @@ player_input = {
 valid_tiles = (".", "+", "x", "~", "*", "_")
 pickup = ("x", "*")
 pushable = ("R")
-n_rows = len(current_world)
-n_cols = len(current_world[0])
 
-#Create World
-reset()
+"""Main game function"""
+def main():
+    # Create World
+    reset()
 
-#Game Loop
-while True:
+    # Check if there is exactly one Laro in the game world
+    player_position = find_object(current_world, "L")
+    if player_position is None:
+        print("Error: No player \"L\" was found in this world!")
+        return
+    elif isinstance(player_position, list) and len(player_position) > 1:
+        print("Error: Multiple player \"L\"s were found in this world!")
+        return
+       
+    # Arg Parse
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", help="Stage file")
+    parser.add_argument("-m", "--moves", help="Moves")
+    parser.add_argument("-o", "--output", help = "Output file")
+    args = parser.parse_args()
 
-    #Checking if outputfile given
-    if args.output and move_queue == None:
-        with open(args.output, "w", encoding='utf-8') as f:
-            if game_data["shrooms"] >= game_data["total_shrooms"]:
-                f.write("CLEAR\n")
-            else:
-                f.write("NO CLEAR\n")
-            f.write(f"{n_rows} {n_cols}\n")
-            f.write("\n".join("".join(row) for row in current_world))
-            f.close()
-        break
+    # Checking if a map file was given
+    if args.file:
+        with open(args.file, encoding='utf-8') as f:
+            rows, cols = [int(x) for x in f.readline().strip().split()]
+            rest = [line.strip() for line in f if line.strip()]
+            world = "".join(rest)
+            global world1
+            world1 = [list(world[i * cols:(i + 1) * cols]) for i in range(rows)] #  <--------------why is world1 grayed out
 
-    elif not args.output:
-        clear_screen()
-        print("\n".join("".join(row) for row in current_world))
-        print()
-        print(f"Shrooms: {game_data["shrooms"]}/{game_data["total_shrooms"]}")
-        print(f"Inventory: {game_data["inventory"]}")
-        print(f"Player tile: {game_data["player_tile"]}\n")
-        
-    if game_data["state"] == "gameover":
-        if game_data["shrooms"] >= game_data["total_shrooms"]:
-            print("You Win!")
-        else:
-            print("Game Over")
+            global n_rows, n_cols
+            n_rows = rows
+            n_cols = cols
 
-    if game_data["state"] == "playing":
-        if move_queue:
-            action = move_queue
-            move_queue = None
-        else:
-            action = input("Move (W, A, S, D) \nPickup (P)\nReset (!) \n").lower()
-    elif game_data["state"] == "gameover":
-        action = input("Reset (!)\n").lower()
+    # Checking if moves are used in advance
+    if args.moves:
+        move_queue = args.moves
+    else:
+        move_queue = None
 
-    for act in action:
-        if act not in player_input:
-            break
-        elif act == "!":
-            reset()
-        elif game_data["state"] == "playing":
-            if act == "p" and game_data["player_tile"] in pickup and game_data["inventory"] == ".":
-                game_data["inventory"] = game_data["player_tile"]
-                game_data["player_tile"] = "."
-            elif act != "p":
-                player_row, player_col = find_object(current_world, "L")
-                current_world[player_row][player_col] = game_data["player_tile"]
-                new_r, new_c, game_data["player_tile"] = player_move((player_row, player_col), player_input[act], current_world)
-                if game_data["player_tile"] == "~":
-                    game_data["state"] = "gameover"
+    # Game Loop
+    while True:
+
+        #C hecking if outputfile given
+        if args.output and move_queue == None:
+            with open(args.output, "w", encoding='utf-8') as f:
+                if game_data["shrooms"] >= game_data["total_shrooms"]:
+                    f.write("CLEAR\n")
                 else:
-                    if game_data["player_tile"] == "+":
-                        game_data["player_tile"] = "."
-                        game_data["shrooms"] += 1
-                        if game_data["shrooms"] >= game_data["total_shrooms"]:
-                            game_data["state"] = "gameover"
-                    current_world[new_r][new_c] = "L"
+                    f.write("NO CLEAR\n")
+                f.write(f"{n_rows} {n_cols}\n")
+                f.write("\n".join("".join(row) for row in current_world))
+                f.close()
+            break
 
+        elif not args.output:
+            clear_screen()
+            print("\n".join("".join(row) for row in current_world))
+            print()
+            print(f"Shrooms: {game_data["shrooms"]}/{game_data["total_shrooms"]}")
+            print(f"Inventory: {game_data["inventory"]}")
+            print(f"Player tile: {game_data["player_tile"]}\n")
+            
+        if game_data["state"] == "gameover":
+            if game_data["shrooms"] >= game_data["total_shrooms"]:
+                print("You Win!")
+            else:
+                print("Game Over")
+
+        if game_data["state"] == "playing":
+            if move_queue:
+                action = move_queue
+                move_queue = None
+            else:
+                action = input("Move (W, A, S, D) \nPickup (P)\nReset (!) \n").lower()
+        elif game_data["state"] == "gameover":
+            action = input("Reset (!)\n").lower()
+
+        for act in action:
+            if act not in player_input:
+                break
+            elif act == "!":
+                reset()
+            elif game_data["state"] == "playing":
+                if act == "p" and game_data["player_tile"] in pickup and game_data["inventory"] == ".":
+                    game_data["inventory"] = game_data["player_tile"]
+                    game_data["player_tile"] = "."
+                elif act != "p":
+                    player_row, player_col = find_object(current_world, "L")
+                    current_world[player_row][player_col] = game_data["player_tile"]
+                    new_r, new_c, game_data["player_tile"] = player_move((player_row, player_col), player_input[act], current_world)
+                    if game_data["player_tile"] == "~":
+                        game_data["state"] = "gameover"
+                    else:
+                        if game_data["player_tile"] == "+":
+                            game_data["player_tile"] = "."
+                            game_data["shrooms"] += 1
+                            if game_data["shrooms"] >= game_data["total_shrooms"]:
+                                game_data["state"] = "gameover"
+                        current_world[new_r][new_c] = "L"
+
+if __name__ == "__main__":
+    main()
